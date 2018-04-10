adf1fc03082b9ebf1274faaf99b1bf1598818921
gioby <31105920+giobyR@users.noreply.github.com>
2018-04-10 16:57:04 +0200

    fixed navbar css layout and added onclick events

--- php/layout/navbar.php
+++ php/layout/navbar.php
@@ -1,27 +1,27 @@
 <div>
 <ul class="navbar">
     <li class="active"><a>Home</a></li>
-    <li><a href='CaricaEventi.loadData(CaricaEventi.EVENTI_PIU_RECENTI)'>Ultimi Eventi Inseriti</a></li>
-    <li><a href='CaricaEventi.loadData(CaricaEventi.EVENTI_PIU_INTERESSANTI)'>Eventi del momento</a></li>
+    <li><a href='#' onclick='CaricaEventi.loadData(CaricaEventi.EVENTI_PIU_RECENTI)'>Ultimi Eventi Inseriti</a></li>
+    <li><a href='#' onclick='CaricaEventi.loadData(CaricaEventi.EVENTI_PIU_INTERESSANTI)'>Eventi del momento</a></li>
     <li class="dropdown">
         <a class="dropdown-button" href='javascript:void(0)'>Categoria Eventi</a>
         <div class="dropdown-content">
-            <a href="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'bambini')">Bambini</a>
-            <a href="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'cinema')">Cinema</a>
-            <a href="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'concerti')">Concerti</a>
-            <a href="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'cultura')">Cultura</a>
-            <a href="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'nightlife')">Nightlife</a>
-            <a href="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'sport')">Sport</a>
-            <a href="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'altro')">Altro</a>
+            <a href='#' onclick="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'bambini')">Bambini</a>
+            <a href='#' onclick="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'cinema')">Cinema</a>
+            <a href='#' onclick="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'concerti')">Concerti</a>
+            <a href='#' onclick="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'cultura')">Cultura</a>
+            <a href='#' onclick="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'nightlife')">Nightlife</a>
+            <a href='#' onclick="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'sport')">Sport</a>
+            <a href='#' onclick="CaricaEventi.loadDataCategoria(CaricaEventi.CATEGORIA,'altro')">Altro</a>
         </div>
     </li>
     <li class="dropdown">
         <a class="dropdown-button" href='javascript:void(0)'>Profilo Personale</a>
         <div class="dropdown-content">
-            <a href='CaricaEventi.loadData(CaricaEventi.ACCOUNT_UTENTE)'>Account</a>
-            <a href='CaricaEventi.loadData(CaricaEventi.EVENTI_INTERESSE_UTENTE)'>Eventi d'interesse</a>
-            <a href='CaricaEventi.loadData(CaricaEventi.EVENTI_PARTECIPAZIONI_UTENTE)'>Partecipazioni</a>
-            <a href='CaricaEventi.loadData(CaricaEventi.EVENTI_CREATI_UTENTE)'>Eventi creati</a>
+            <a href='#' onclick='CaricaEventi.loadData(CaricaEventi.ACCOUNT_UTENTE)'>Account</a>
+            <a href='#' onclick='CaricaEventi.loadData(CaricaEventi.EVENTI_INTERESSE_UTENTE)'>Eventi d'interesse</a>
+            <a href='#' onclick='CaricaEventi.loadData(CaricaEventi.EVENTI_PARTECIPAZIONI_UTENTE)'>Partecipazioni</a>
+            <a href='#' onclick='CaricaEventi.loadData(CaricaEventi.EVENTI_CREATI_UTENTE)'>Eventi creati</a>
             <a>Logout</a>
         </div>
     </li>
