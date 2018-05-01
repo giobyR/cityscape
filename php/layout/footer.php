029b5140617e896bf4379b2e5c994db319749bf1
gioby <31105920+giobyR@users.noreply.github.com>
2018-05-01 18:30:49 +0200

    pagina_evento layout changes

--- php/layout/footer.php
+++ php/layout/footer.php
@@ -1,8 +1,12 @@
-<div>
-    <address>
-        Creato da Popitanu Silviu Roberto<br>
-        Contatti: <a href="<mailto:>popitanu_roberto@hotmail.com</mailto:>">Popitanu S. Roberto</a><br>
-        Indirizzo: Sarzana(SP),Italia<br>
-    </address>
-    <p><a href="../html/condizioniUso.html">Termini e condizioni d'uso</a></p>
+<div id="footer-contenitore">
+    <div id="titoloFooter">
+        <p id="marchio"> Cityscape ®2018</p>
+        <p>Creato da Popitanu Silviu Roberto</p>
+    </div>
+    <div id="indirizzoFooter">
+        <address>
+            Contatti: <a href="<mailto:>popitanu_roberto@hotmail.com</mailto:>">Popitanu S. Roberto</a><br>
+        </address>
+        <p><a href="../html/condizioniUso.html">Termini e condizioni d'uso</a></p>
+    </div>
 </div>
--- php/pagina_evento.php
+++ php/pagina_evento.php
@@ -16,10 +16,9 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/css/navbar.css">
-    <link rel="stylesheet" href="/css/sidebar.css">
-    <link rel="stylesheet" href="/css/layout_evento.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/pagina_evento.css">
 
     <script type="text/javascript" src="/js/effects.js"></script>
     <script type="text/javascript" src="/js/caricaEventi.js"></script>
@@ -30,34 +29,56 @@
     <title>Informazioni Evento</title>
 </head>
 <body>
-     <?php
+    <nav>
+        <?php
+            include DIR_LAYOUT.'navbar.php';
+        ?>
+    </nav>
+    <div id="poster">
+        <?php
         echo "<img alt='immagine copertina evento' src=/images/eventi/".$_GET['poster'].">";
-     ?>
-    <p><?php echo $_GET['titolo']; ?></p>
-    <p><?php echo $_GET['descrizione']; ?></p>
-    <p>Luogo evento: <?php echo $_GET['luogo']; ?></p>
-    <p>Data Evento: <?php echo $_GET['data']; ?></p>
-    <p>Costo evento:<?php 
-                        if($_GET['prezzo']==0){
-                            echo "gratis";
-                        }else{
-                            echo $_GET['prezzo']; 
-                        }
-                    ?>
-    </p>
-    <p>Numero massimo di partecipanti: <?php 
-                                            if($_GET['maxPartecipanti']==0){
-                                                echo "nessun limite";
-                                            }else{
-                                                echo $_GET['maxPartecipanti'];
-                                            }     
-                                        ?>
-    </p>
-    <label>Hai un codice referral?</label>
-    <input type='radio' id="sceltaSI" name='selezioneReferral' checked>NO</input>
-    <input type='radio' id="sceltaNO" name='selezioneReferral'>SI</input>
-    Codice Referral<input type='text' id='referral' name='referral' disabled>
-    <button id='buttonPartecipa'>Partecipa</button>
+        ?>
+    </div>
+    <header id="titolo">
+        <h1>
+            <?php echo $_GET['titolo']; ?>   
+        </h1>
+    </header>     
+    <p id="descrizione"><?php echo $_GET['descrizione']; ?></p>
+    <div id="luogo-data">
+        <p>Luogo evento: <?php echo $_GET['luogo']; ?></p>
+        <p>Data Evento: <?php echo $_GET['data']; ?></p>
+    </div>
+    <div id="costo">
+        <p>Costo evento:<?php 
+                            if($_GET['prezzo']==0){
+                                echo "gratis";
+                            }else{
+                                echo $_GET['prezzo']; 
+                            }
+                        ?>
+        </p>
+        <p>Numero massimo di partecipanti: <?php 
+                                                if($_GET['maxPartecipanti']==0){
+                                                    echo "nessun limite";
+                                                }else{
+                                                    echo $_GET['maxPartecipanti'];
+                                                }     
+                                            ?>
+        </p>
+    </div>
+    <div id="referral-contenitore">
+        <div id="selezione-referral">
+            <label>Hai un codice referral?</label>
+            <input type='radio' id="sceltaSI" name='selezioneReferral' checked>NO</input>
+            <input type='radio' id="sceltaNO" name='selezioneReferral'>SI</input>
+        </div>
+        <span >
+            <label for="referral">Codice Referral:</label>
+            <input type='text' id='referral' name='referral' disabled>
+        </span>
+        <button id='buttonPartecipa'>Partecipa</button>
+    </div>
     <p id='errMsg'></p>
     <?php
         $result= getReferral($_GET['creatore']); 
@@ -71,5 +92,10 @@
         echo "document.getElementById('sceltaNO').addEventListener('click',abilitaReferral);";
         echo "</script>";
     ?>
+    <footer>
+            <?php
+                include DIR_LAYOUT.'footer.php';
+            ?>
+    </footer>
 </body>
 </html>
