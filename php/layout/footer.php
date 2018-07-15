74ab94eadba9c89a9bc662cd6eb7b59c604a262b
gioby <31105920+giobyR@users.noreply.github.com>
2018-07-15 10:19:39 +0200

    modified documentation, general use condition and footer

--- php/layout/footer.php
+++ php/layout/footer.php
@@ -1,11 +1,10 @@
 <div id="footer-contenitore">
     <div id="titoloFooter">
         <p id="marchio"> Cityscape ®2018</p>
-        <p>Creato da Popitanu Silviu Roberto</p>
     </div>
     <div id="indirizzoFooter">
         <address>
-            Contatti: <a href="mailto:popitanu_roberto@hotmail.com">Popitanu S. Roberto</a><br>
+            Autore: <a href="mailto:popitanu_roberto@hotmail.com">Popitanu S. Roberto</a><br>
         </address>
         <p><a href="../html/condizioniUso.html">Termini e condizioni d'uso</a></p>
         <a href="/html/documentazione.html">Documentazione</a>
